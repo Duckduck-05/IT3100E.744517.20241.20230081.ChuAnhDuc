6cab2e390d952fac61ab8677067ff34ad51be71b
Chu Anh Duc <chuanhduc09@gmail.com>
2024-09-28 00:45:21 +0700

    Complete 2.2.7.1

---
+++ Lab1/2.2.7/ChoosingOption.java
@@ -0,0 +1,12 @@
+
+import javax.swing.JOptionPane;
+
+public class ChoosingOption {
+    public static void main(String[] args) {
+        int option = JOptionPane.showConfirmDialog(null, 
+                    "Do you want to change to the first class ticket?");
+        JOptionPane.showMessageDialog(null, "You're chosen: "
+                        + (option == JOptionPane.YES_OPTION?"Yes":"No"));
+        System.exit(0);
+    }
+}
