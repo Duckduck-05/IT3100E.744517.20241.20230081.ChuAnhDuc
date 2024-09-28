6d481c5fe4dfe7be8bc791286a710cc2c556114c
Chu Anh Duc <chuanhduc09@gmail.com>
2024-09-28 09:39:12 +0700

    Complete 6.2

--- Lab1/2.2.7/ChoosingOption.java
+++ Lab1/2.2.7/ChoosingOption.java
@@ -4,7 +4,7 @@ import javax.swing.JOptionPane;
 public class ChoosingOption {
     public static void main(String[] args) {
         int option = JOptionPane.showConfirmDialog(null, 
-                    "Do you want to change to the first class ticket?");
+                    "Do you want to change to the first class ticket?", "Choose ticket", JOptionPane.YES_NO_OPTION);
         JOptionPane.showMessageDialog(null, "You're chosen: "
                         + (option == JOptionPane.YES_OPTION?"Yes":"No"));
         System.exit(0);
