a17849f56ad3ab54846487d01b8f3f55e46a45c0
Chu Anh Duc <chuanhduc09@gmail.com>
2024-12-12 17:11:26 +0700

    Update GUIProject

---
+++ OtherProjects/GUIProject/src/hust/soict/dsai/swing/hust/soict/dsai/swing/AWTAccumlator.java
@@ -0,0 +1,44 @@
+package hust.soict.dsai.swing;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+public class AWTAccumlator extends Frame {
+    private TextField tfInput;
+    private TextField tfOutput;
+    private int sum = 0;
+
+    public AWTAccumlator() {
+        setLayout(new GridLayout(2, 2));
+
+        add(new Label("Enter an Integer: "));
+
+        tfInput = new TextField(10);
+        add(tfInput);
+        tfInput.addActionListener(new TFInputListener());
+        
+        add(new Label("The Accumulated Sum is: "));
+
+        tfOutput = new TextField(10);
+        tfOutput.setEditable(false);
+        add(tfOutput);
+
+        setTitle("AWT Accumulator");
+        setSize(350, 120);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new AWTAccumlator();
+    }
+
+    private class TFInputListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent evt) {
+            int numberIn = Integer.parseInt(tfInput.getText());
+            sum += numberIn;
+            tfInput.setText("");
+            tfOutput.setText(sum + "");
+        }
+    }
+    
+}
---
+++ OtherProjects/GUIProject/src/hust/soict/dsai/swing/hust/soict/dsai/swing/SwingAccumulator.java
@@ -0,0 +1,48 @@
+package hust.soict.dsai.swing;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
+
+public class SwingAccumulator extends JFrame {
+    private JTextField tfInput;
+    private JTextField tfOutput;
+    private int sum = 0;
+
+    public SwingAccumulator() {
+        Container cp = getContentPane();
+        cp.setLayout(new GridLayout(2, 2));
+
+        cp.add(new Label("Enter an Integer: "));
+
+        tfInput = new JTextField(10);
+        cp.add(tfInput);
+        tfInput.addActionListener(new TFInputListener());
+        
+        cp.add(new Label("The Accumulated Sum is: "));
+
+        tfOutput = new JTextField(10);
+        tfOutput.setEditable(false);
+        cp.add(tfOutput);
+
+        setTitle("AWT Accumulator");
+        setSize(350, 120);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new SwingAccumulator();
+    }
+
+    private class TFInputListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent evt) {
+            int numberIn = Integer.parseInt(tfInput.getText());
+            sum += numberIn;
+            tfInput.setText("");
+            tfOutput.setText(sum + "");
+
+        }
+    }
+}
